ff3741f75a861be0ce86752f1accb33ae5151bba
YeB29 <74351462+YeB29@users.noreply.github.com>
2022-01-11 13:48:49 +0100

    chat

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,20 +1,34 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace SignalRChat.Hubs
 {
- 
     public class ChatHub : Hub
     {
-        public Task JoinRoom(string roomId)
+        public async Task SendMessage(string user, string message)
         {
-            return Groups.AddToGroupAsync(Context.ConnectionId, roomId);
-        }
+            
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        // }
+        // public override Task OnConnected()  
+        // {  
+        // ConnectedUser.Ids.Add(Context.ConnectionId);  
+        // return base.OnConnected();  
+        // }  
+        // public override Task OnDisconnected()  
+        // {  
+        // ConnectedUser.Ids.Remove(Context.ConnectionId);  
+        // return base.OnDisconnected();  
+        // }  
+        //   }  
+        //     }
+        // }
+        // public static class ConnectedUser  
+        // {  
+        // public static List<string> Ids = new List<string>();  
 
-        public Task LeaveRoom(string roomId)
-        {
-            return Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId);
-        }
-    }
-}
+        // public Task OnConnected(){
+
+        // }
+        }        }}
