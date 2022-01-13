1b4698e12540e984c6c1e9c521b04ae49bce3e00
Serdin19 <serdin13@outlook.com>
2022-01-13 20:48:01 +0100

    Design

--- Models/ModeratorEditModel.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Webdevelopment_Project{
-public class ModeratorEditModel
-{
-    public string RoleId { get; set; }
-    public string RoleName { get; set; }
-    public bool Selected { get; set; }
-}
-}
