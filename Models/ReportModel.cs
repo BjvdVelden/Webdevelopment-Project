ee425545061b216b1137aa056e474375c36f7fb2
Serdin19 <serdin13@outlook.com>
2022-01-16 13:13:12 +0100

    Report functie weg

--- Models/ReportModel.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Webdevelopment_Project.Models{
-public class ReportModel
-{
-    public int Id { get; set; }
-    public string Naam { get; set; }
-    public string Reden { get; set; }
-}
-}
