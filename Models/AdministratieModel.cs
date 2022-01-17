90ba9b90c304cad50820b41be98f69423a713672
DucklessDuck <nguyenkevin39@gmail.com>
2022-01-17 09:41:25 +0100

    Added Create, Edit, Delete and PUT
    Co-authored-by: BjvdVelden <BjvdVelden@users.noreply.github.com>

--- Models/AdministratieModel.cs
+++ Models/AdministratieModel.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 namespace Administratie.Models
 {
     public class AdministratieModel
-    {
+     {
         public int clientid { get; set; }
         public string volledigenaam { get; set; }
         public string IBAN { get; set; }
         public int BSN { get; set; }
         public string gebdatum{ get; set; }
+
     }
 
 }
